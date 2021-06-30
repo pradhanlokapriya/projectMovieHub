82003536d9b0708be4e9f337187c77619e6e01f1
Lokapriya Pradhan <pradhanlokapriya@gmail.com>
2021-06-30 15:57:05 +0530

    1stcommit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import Series from "./Pages/Series/Series";
 import Movies from "./Pages/Movies/Movies";
 import Search from "./Pages/Search/Search";
 
-
-
 function App() {
   return (
     <BrowserRouter>
